c1868b747552c2b279c41c8f93624618fd84c338
naiah <nastiasunrise@gmail.com>
2025-07-19 15:07:46 +0200

    hw8: add partitioning key

--- hw8/src/hw8/tweet_producer.py
+++ hw8/src/hw8/tweet_producer.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import random
 import signal
@@ -37,20 +38,21 @@ class TweetProducer:
         if not_delivered_messages:
             logger.error(f"Failed to deliver messages to kafka: number={not_delivered_messages}")
 
-
     def _send_message(self, tweet: Tweet):
+        partition_key = tweet.created_at.strftime("%d_%m_%Y_%H_%M")
         self._producer.produce(
             topic="tweets",
+            key=json.dumps({"created_at": partition_key}),
             value=tweet.model_dump_json().encode('utf-8'),
             callback=_message_sent_callback
         )
 
-    def send_tweets_from_steam(self, stream: Iterable[Tweet], *, max_batch_size: int = 15):
+    def send_tweets_from_steam(self, stream: Iterable[Tweet], *, rps: int = 15, rate_limit: int = 20):
         while True:
             if not self._active:
                 return
 
-            for batch_size, tweet in enumerate(stream):
+            for msg_count, tweet in enumerate(stream):
                 try:
                     self._send_message(tweet)
                     time.sleep(random.uniform(0.066, 0.1))
@@ -59,7 +61,7 @@ class TweetProducer:
                     self._producer.flush(timeout=KAFKA_FLUSH_TIMEOUT)
                     self._send_message(tweet=tweet)
                     break
-                if batch_size >= max_batch_size:
+                if msg_count >= rps:
                     break
             else:
                 break
