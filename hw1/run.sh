14c83432e6bab78b5fea1145a9da2b7726ccd434
naiah <nastiasunrise@gmail.com>
2025-07-22 12:03:37 +0200

    update infrastructure

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+docker-compose down
+
+if test -d db; then
+  echo "Cleaning up volumes..."
+  rm -rf db/
+fi
+
 if test -f .env; then
   docker-compose build data-loader
 
   docker-compose up -d database
-  docker-compose up data-loader
+  docker-compose run data-loader
+  docker-compose down
 else
   echo "Please, create .env from .env.template"
 fi
