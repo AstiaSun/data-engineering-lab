73a891c3b560245c2f823efc1303faf4aaa1cf9b
naiah <nastiasunrise@gmail.com>
2025-07-22 12:03:03 +0200

    update database wrapper

--- hw1/src/db.py
+++ hw1/src/db.py
@@ -1,10 +1,12 @@
+from functools import lru_cache
 from typing import Self, Any
 
-from sqlalchemy import create_engine, Connection, Engine, text
+from sqlalchemy import create_engine, Connection, Engine, text, select, update
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
 from .constants import SQLALCHEMY_DB_URL
-from .models import Base
+from .models import Base, Interest, Location, Campaign
 
 
 class Singleton(type):
@@ -22,7 +24,7 @@ class DBSession(metaclass=Singleton):
         self._session: Session | None = None
 
     def __enter__(self) -> Self:
-        self._session = Session(self._engine).__enter__()
+        self._session = Session(self._engine, expire_on_commit=False).__enter__()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -46,8 +48,38 @@ class DBSession(metaclass=Singleton):
 
     def execute(self, query: str) -> Any:
         statement = text(query)
-        return self.session.execute(statement)
+        result = self.session.execute(statement)
+        self._session.commit()
+        return result
 
     def insert_batch(self, batch: list[Base]):
-        self._session.bulk_save_objects(batch)
-        self._session.commit()
+        self.session.bulk_save_objects(batch)
+        self.session.commit()
+
+    @lru_cache(maxsize=20)
+    def get_interest(self, interest: str) -> Interest:
+        try:
+            statement = select(Interest).where(Interest.Field == interest)
+            return self.session.execute(statement).scalar_one()
+        except NoResultFound:
+            new_interest = Interest(Field=interest)
+            self.session.add(new_interest)
+            self.session.flush()
+            return new_interest
+
+    @lru_cache(maxsize=20)
+    def get_location(self, location: str) -> Location:
+        try:
+            statement = select(Location).where(Location.Country == location)
+            return self.session.execute(statement).scalar_one()
+        except NoResultFound:
+            new_location = Location(Country=location)
+            self.session.add(new_location)
+            self.session.flush()
+            return new_location
+
+    def update_campaigns(self, fields_by_id: dict[int, dict[str, Any]]):
+        for campaign_id, fields in fields_by_id.items():
+            statement = update(Campaign).where(Campaign.CampaignID==campaign_id).values(**fields)
+            self.session.execute(statement)
+        self.session.commit()
