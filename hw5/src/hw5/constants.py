6ccbfb4fbe1ec440c06af5dcfb054804642fed31
naiah <nastiasunrise@gmail.com>
2025-07-24 03:37:32 +0200

    hw5: add requests to mongo

---
+++ hw5/src/hw5/constants.py
@@ -0,0 +1,9 @@
+import os
+
+DATABASE_NAME = "AdEvents"
+
+MONGODB_USERNAME = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
+MONGODB_PASSWORD = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+MONGODB_HOST = os.environ.get("MONGODB_HOST", "localhost")
+
+MONGODB_URI = f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOST}:27017/?authSource=admin"
