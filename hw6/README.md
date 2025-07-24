78618939042794ec41b4a6cecedad9bcad3dc076
naiah <nastiasunrise@gmail.com>
2025-07-24 23:06:47 +0200

    setup infrastructure

---
+++ hw6/README.md
@@ -0,0 +1,9 @@
+### How to run:
+
+1. Place CSV file to dataset folder: `hw6/dataset/amazon_reviews.csv`
+2. Open terminal and change the current working directory to `hw6`
+3. Create an `.env` file from `.env.template` 
+4. Run bash script:
+    ```commandline
+    $ chmod +x run.sh && ./run.sh
+    ```
---
+++ hw6/src/hw6/constants.py
@@ -0,0 +1,12 @@
+import os
+
+DATASET_PATH = "./dataset"
+DATABASE_NAME = "reviews"
+
+MONGODB_USERNAME = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
+MONGODB_PASSWORD = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+MONGODB_HOST = os.environ.get("MONGODB_HOST", "localhost")
+
+MONGODB_URI = (
+    f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOST}:27017/?authSource=admin"
+)
