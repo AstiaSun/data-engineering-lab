612ca77dec69d8c7a257fabf43803ba30c909892
naiah <nastiasunrise@gmail.com>
2025-08-02 17:07:20 +0200

    hw10: downgrade python version and update table schema

--- hw10/docker/init_table.sql
+++ hw10/docker/init_table.sql
@@ -2,9 +2,9 @@ CREATE KEYSPACE IF NOT EXISTS wikimedia
 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
 
 CREATE TABLE IF NOT EXISTS wikimedia.page_create (
-    id UUID PRIMARY KEY,
-    domain TEXT,
-    title TEXT,
-    user TEXT,
-    timestamp TIMESTAMP
+    user_id int,
+    domain text,
+    page_title text,
+    created_at timestamp,
+    PRIMARY KEY (user_id, created_at)
 );
--- hw10/run.sh
+++ hw10/run.sh
@@ -9,9 +9,20 @@ docker-compose exec kafka bash -c "kafka-topics.sh --bootstrap-server localhost:
 
 docker-compose up -d spark spark-worker cassandra
 
+
+CONTAINER_NAME=$(docker inspect --format '{{.Name}}' $(docker-compose ps -q cassandra) | sed 's|/||')
+
+echo "Container name is: $CONTAINER_NAME"
+until [ "$(docker inspect -f '{{.State.Health.Status}}' $CONTAINER_NAME)" == "healthy" ]; do
+  echo "Waiting for Cassandra to become healthy..."
+  sleep 5
+done
+
 # create tables in cassandra
 docker-compose cp ./docker/init_table.sql cassandra:/tmp/init.sql
 docker-compose exec cassandra bash -c 'cqlsh -u $CASSANDRA_USER -p $CASSANDRA_PASSWORD -f /tmp/init.sql'
 
 # run pipeline
 docker-compose up pipeline
+#docker-compose exec pipeline bash -c 'poetry run python -m src.hw10.data_filtering'
+#docker-compose exec pipeline bash -c 'poetry run python -m src.hw10.data_loading'
