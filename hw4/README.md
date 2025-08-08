085f5b32bf3920e201f47c0fccb3b5d1e58000ef
naiah <nastiasunrise@gmail.com>
2025-08-08 22:43:25 +0200

    hw4: infrastructure setup

---
+++ hw4/README.md
@@ -0,0 +1,11 @@
+### How to run:
+
+1. Open terminal and change the current working directory to `hw4`
+2. Create a .env file from .env.template
+3. Run bash script:
+    ```commandline
+    $ chmod +x run.sh && ./run.sh
+    ```
+
+### Comments
+Cassandra needs about 1-2 minutes to become healthy.
---
+++ hw4/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker-compose build pipeline
+docker-compose up -d cassandra
+
+CONTAINER_NAME=$(docker inspect --format '{{.Name}}' $(docker-compose ps -q cassandra) | sed 's|/||')
+
+until [ "$(docker inspect -f '{{.State.Health.Status}}' $CONTAINER_NAME)" == "healthy" ]; do
+  echo "Waiting for Cassandra to become healthy..."
+  sleep 5
+done
+
+docker-compose cp ./cql/setup.cql cassandra:/tmp/setup.cql
+docker-compose exec cassandra bash -c 'cqlsh -u $CASSANDRA_USER -p $CASSANDRA_PASSWORD -f /tmp/setup.cql'
+
+docker-compose up pipeline
